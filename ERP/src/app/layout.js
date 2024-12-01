f49544d23c3a3743e6792430684cad171d5b4a83
KushalRastogi20 <138278696+KushalRastogi20@users.noreply.github.com>
2024-12-01 18:47:48 +0530

    nav and footer

--- ERP/src/app/layout.js
+++ ERP/src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-// import Footer from '@/components/Footer'
-// import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,9 +18,9 @@ export default function RootLayout({ children }) {
     
       <body className={inter.className}>
         
-        {/* <Navbar/> */}
+        <Navbar/>
         {children}
-        {/* <Footer/> */}
+        <Footer/>
 
       </body>
     </html>
